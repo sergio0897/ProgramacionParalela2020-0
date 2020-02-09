9a9b4673f6fc270e1e44f010236806673aed8d88
sergio <sergio@sergio-VirtualBox>
2020-02-08 22:16:16 -0500

    Fix: Actualizacion de trabajos practicos, renames

---
+++ Trabajo_Práctico_01/src/Trabajo_Práctico_01.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+int main(int argc, char* argv[])
+{
+
+    MPI_Init(&argc, &argv);
+
+    int rangoBase = 0;
+
+    int miRango;
+    MPI_Comm_rank(MPI_COMM_WORLD, &miRango);
+
+
+    int resultado = 0;
+    MPI_Reduce(&miRango, &resultado, 1, MPI_INT, MPI_MAX, rangoBase, MPI_COMM_WORLD);
+
+    if(miRango == rangoBase)
+    {
+        printf("El m�ximo de todos es %d.", resultado);
+    }
+
+    MPI_Finalize();
+
+    return EXIT_SUCCESS;
+}
